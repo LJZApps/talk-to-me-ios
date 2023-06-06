fd2ca5d2b65735f7bfe8098b9a7c05b99137c766
leonzapke <leonzapke@gmail.com>
2023-06-06 11:24:46 +0200

    Initial Commit

---
+++ Talk to me/Talk_to_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talk_to_meApp.swift
+//  Talk to me
+//
+//  Created by Leon Zapke on 06.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Talk_to_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
